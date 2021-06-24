e70c2cc10b36b392e0fe91b67ddb092e0a2740fc
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-06-25 01:08:53 +0500

    Add Swagger and Add Item route

--- controllers/items.js
+++ controllers/items.js
@@ -1,16 +1,29 @@
-const items = require('../Items')
+let items = require("../Items")
+const { v4: uuidv4 } = require("uuid")
 
-const getItems = (req,reply) => {
-	reply.send(items)
+const getItems = (req, reply) => {
+  reply.send(items)
 }
 
-const getItem = (req,reply) => {
-	const {id} = req.params
-	const item = items.find(item => item.id === id)
-	reply.send(item)
+const getItem = (req, reply) => {
+  const { id } = req.params
+  const item = items.find((item) => item.id === id)
+  reply.send(item)
+}
+
+const addItem = (req, reply) => {
+  const { name } = req.body
+  const item = {
+    id: uuidv4(),
+    name,
+  }
+  console.log(item)
+  items = [...items, item]
+  reply.code(201).send(item)
 }
 
 module.exports = {
-	getItems,
-	getItem
+  getItems,
+  getItem,
+  addItem,
 }
--- routes/items.js
+++ routes/items.js
@@ -1,42 +1,61 @@
-const {getItem,getItems} = require('../controllers/items')
+const { getItem, getItems, addItem } = require("../controllers/items")
 
 const Item = {
-	type: 'object',
-	properties: {
-		id: {type:'integer'},
-		name: {type:'string'}
-	}
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    name: { type: "string" },
+  },
 }
 
 // Options to get all items
 const getItemsOpts = {
-	schema: {
-		response: {
-			200: {
-				type: 'array',
-				items: Item
-			}
-		}
-	},
-	handler: getItems
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: Item,
+      },
+    },
+  },
+  handler: getItems,
 }
 
 const getItemOpts = {
-	schema: {
-		response: {
-			200: Item
-		}
-	},
-	handler: getItem
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: getItem,
 }
 
-function itemRoutes (fastify,options,done) {
-	//Get all items
-	fastify.get('/items', getItemsOpts)
-	
-	//Get single items
-	fastify.get('/items/:id',getItemOpts)
-	done()
+const postItemOpts = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: { type: "string" },
+      },
+    },
+    response: {
+      201: Item,
+    },
+  },
+  handler: addItem,
 }
 
-module.exports =itemRoutes
+function itemRoutes(fastify, options, done) {
+  //Get all items
+  fastify.get("/items", getItemsOpts)
+
+  //Get single items
+  fastify.get("/items/:id", getItemOpts)
+
+  //Add Item
+  fastify.post("/items", postItemOpts)
+  done()
+}
+
+module.exports = itemRoutes
